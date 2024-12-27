90946ee86bb725d8190f04aaca339defc41ef2b9
Charan <charanv@devtools.in>
2024-12-27 12:05:56 +0000

    updated button

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
+import ForgeReconciler, { Text, Button } from '@forge/react';
 import { invoke } from '@forge/bridge';
 
 const App = () => {
   const [data, setData] = useState(null);
+
   useEffect(() => {
     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
   }, []);
+
+  const handleClick = () => {
+    alert('Primary button clicked!');
+  };
+
   return (
     <>
       <Text>DevTools.</Text>
       <Text>{data ? data : 'Loading...'}</Text>
+      <Button appearance="primary" onClick={handleClick}>
+        Primary Button
+      </Button>
     </>
   );
 };
